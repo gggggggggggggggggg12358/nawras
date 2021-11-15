99285d7cd4cfbb31bfbcd5d084c34788ab247f2d
gggggggggggggggggg12358 <94399582+gggggggggggggggggg12358@users.noreply.github.com>
2021-11-15 21:44:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ signUpBtn2.addEventListener('click', () => {
     window.alert("اااسف الباسوورد بس مع حبيبتي وما بقدر اسجل غيرها")
 });
 const error = function (){
-     if(userInput.value!=="nawras" || passInput.value!==161199){
+     if(userInput.value!=="nawras" ){
         console.log("ali")
         window.alert("اااسف الباسوورد بس مع حبيبتي وما  بفوت غيرها")
         window.location.reload()
